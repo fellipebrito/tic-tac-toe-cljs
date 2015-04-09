cdb9236ae5e3f7960ae87024675d11adf8ca1979
Fellipe Brito <fellipe@devlandia.net>
2015-04-09 17:20:13 +0000

    Improvess the game layout

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.clojure/clojurescript "0.0-2850"]
                  [figwheel "0.2.5-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [jayq "2.5.4"]
                  [sablono "0.3.4"]
                  [org.omcljs/om "0.8.8"]]
 
--- src/ttt/core.cljs
+++ src/ttt/core.cljs
@@ -5,6 +5,7 @@
             [om.dom :as dom :include-macros true]
             [clojure.string :as str]
             [clojure.data :as dt]
+            [jayq.core :refer [$ css html]]
             [cljs.core.async :refer [put! chan <!]]))
 
 (enable-console-print!)
@@ -59,10 +60,8 @@
     (if (not= \- (get board place))
       (do (js/alert "Invalid move") board)
       (let [board-after-move (str (subs board 0 place) player (subs board (inc place)))]
-          (if (draw? board-after-move)
-            (do (js/alert (str "It's a Draw!")) (swap! app-state assoc :player "x") (swap! app-state assoc :board (new-board)))
             (do (swap! app-state assoc :player (if (= "x" player) "o" "x")) ;; change player for the next move
-                (swap! app-state assoc :board board-after-move)))))]
+                (swap! app-state assoc :board board-after-move))))]
   (if human?
     (if (not (win? (:board board-after-human)))
     (do (move (:board board-after-human) (robot-move (:board board-after-human)) (if (= "x" player) "o" "x") false))
@@ -79,33 +78,46 @@
     (swap! app-state assoc :board board)))
 
 (defn show-board [owner data]
-  (if (win? (:board data))
     (do
       (dom/div nil
-        (dom/div #js {:className "alert"}
-                 (dom/div #js {:className "finalMessage"} (str (if (= "x" (:player data)) (rand-nth ["What a shame!" "SuperBot wins!" "It was easy!"]) (rand-nth ["Adrian, we did it!" "This is happiness!" "Sweet!"]))))
-                 (dom/button #js {:className "restart" :onClick #(om/set-state! owner :board (restart data))} "Play Again!"))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 0 (:player data) true))} (subs (:board data) 0 1))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 1 (:player data) true))} (subs (:board data) 1 2))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 2 (:player data) true))} (subs (:board data) 2 3))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 3 (:player data) true))} (subs (:board data) 3 4))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 4 (:player data) true))} (subs (:board data) 4 5))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 5 (:player data) true))} (subs (:board data) 5 6))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 6 (:player data) true))} (subs (:board data) 6 7))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 7 (:player data) true))} (subs (:board data) 7 8))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 8 (:player data) true))} (subs (:board data) 8 9))))
-    (do
-      (dom/div nil
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 0 (:player data) true))} (subs (:board data) 0 1))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 1 (:player data) true))} (subs (:board data) 1 2))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 2 (:player data) true))} (subs (:board data) 2 3))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 3 (:player data) true))} (subs (:board data) 3 4))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 4 (:player data) true))} (subs (:board data) 4 5))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 5 (:player data) true))} (subs (:board data) 5 6))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 6 (:player data) true))} (subs (:board data) 6 7))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 7 (:player data) true))} (subs (:board data) 7 8))
-        (dom/button #js {:onClick #(om/set-state! owner :board (move (str (:board data)) 8 (:player data) true))} (subs (:board data) 8 9))))
-  ))
+        ;; (if (not (win? (:board data)))
+        (if (win? (:board data))
+          (do
+              (dom/div #js {:className "alert"}
+                      (dom/div #js {:className "finalMessage animated tada"} (str (if (= "x" (:player data)) (rand-nth ["What a shame!" "SuperBot wins!" "It was easy!"]) (rand-nth ["Adrian, we did it!" "This is happiness!" "Sweet!"]))))
+                      (dom/button #js {:className "restart animated infinite pulse" :onClick #(om/set-state! owner :board (restart data))} "Play Again!"))))
+        (if (draw? (:board data))
+          (do
+              (dom/div #js {:className "col-lg-8 col-lg-offset-2 alert"}
+                      (dom/div #js {:className "finalMessage animated tada"} "It is a Draw!")
+                      (dom/button #js {:className "restart animated infinite pulse" :onClick #(om/set-state! owner :board (restart data))} "Play Again!"))))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4 col-md-offset-3 col-lg-offset-3"}
+                (dom/button #js {:className (subs (:board data) 0 1)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 0 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4"}
+                (dom/button #js {:className (subs (:board data) 1 2)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 1 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4"}
+                (dom/button #js {:className (subs (:board data) 2 3)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 2 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4 col-md-offset-3 col-lg-offset-3"}
+                (dom/button #js {:className (subs (:board data) 3 4)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 3 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4"}
+                (dom/button #js {:className (subs (:board data) 4 5)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 4 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4"}
+                (dom/button #js {:className (subs (:board data) 5 6)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 5 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4 col-md-offset-3 col-lg-offset-3"}
+                (dom/button #js {:className (subs (:board data) 6 7)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 6 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4"}
+                (dom/button #js {:className (subs (:board data) 7 8)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 7 (:player data) true))}))
+        (dom/div #js {:className "col-lg-2 col-md-2 col-sm-4 col-xs-4"}
+                (dom/button #js {:className (subs (:board data) 8 9)
+                                  :onClick #(om/set-state! owner :board (move (str (:board data)) 8 (:player data) true))})))))
 
 (defn game [data owner]
   (reify
@@ -118,4 +130,4 @@
       (show-board owner data))))
 
 (om/root game app-state
-  {:target (.getElementById js/document "app")})
+  {:target (.getElementById js/document "game")})
